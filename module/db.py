05ea69e33ca5ed3915fea42cfa12155e8a067c51
yusiang <10656010@ntub.edu.tw>
2021-10-03 23:43:00 +0800

    移除 - 多餘檔案

--- module/db.py
+++ module/db.py
@@ -36,17 +36,13 @@ class DB():
                 password=DB.__password,
                 charset="utf8")
             if connection.is_connected():
-                # 顯示資料庫版本
-                # db_Info = connection.get_server_info()
-                # print("資料庫版本：", db_Info)
-                # 執行傳入的sql 指令
                 cursor = connection.cursor(dictionary=True)
                 if(isinstance(sqlstr, list)):
                     result = {}
-                    for sqlstrItem in sqlstr:                        
+                    for sqlstrItem in sqlstr:
                         cursor.execute(sqlstrItem["sql"])
                         rows = cursor.fetchall()
-                        result[sqlstrItem["name"]]=rows                        
+                        result[sqlstrItem["name"]] = rows
                     return {"success": True, "data": result}
                 else:
                     if(type == DB.create or type == DB.update):
@@ -57,15 +53,8 @@ class DB():
                         cursor.execute(sqlstr)
                         rows = cursor.fetchall()
                         return {"success": True, "data": rows}
-
                 cursor.close()
                 connection.close()
-                print("enter close")
-
         except Error as e:
             print("資料庫連接失敗：", e)
-            # print(cursor)
-            # cursor.close()
-            # connection.close()
-            print(e)
             return {"success": False, "data": str(e)}
